65ad7576694800a6cfa28199e0f952f2cd87a94d
Maecenass <maecenasss@gmail.com>
2022-11-26 13:57:04 +0200

    create clean users component & component UsersContainer to request to API server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,77 +1,51 @@
-import axios from "axios";
-import React, { useCallback } from "react";
+import React from "react";
 import styles from './users.module.css'
 import userPhoto from 'C:/ИТ/React/It-Kamasutra/react-way-of-samurai/src/assets/img/profile.png'
 
-//create class Users
-class Users extends React.Component {
+let Users = (props) => {
+    //create pages counter and create pages area & округляємо до цілого
+    let pagesCount = Math.ceil (props.totalUsersCount/props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
     
-        //componentDidMount use for get request to server
-        componentDidMount () {
-        //перед відрисовкою утворюємо юзерів, переносимо state із user-reducer 
-        //take request to server to get users state & after get state - setUsers
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-                .then(response => {
-                this.props.setUsers(response.data.items);
-            });
-        }
-    
-        //create method to change pages with onClick
-        onPageChanged = (pageNumber) => {
-            this.props.setCurrentPage(pageNumber);
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-                .then(response => {
-                this.props.setUsers(response.data.items);
-            });
-        }
+    return <div className='app-wrapper app-wrapper-content'>
+    {/*робимо так щоб відображося виділення сторінки юзера що відкрита*/}
+    <div>
+     {pages.map (p => {
+         return  <span onClick = {() => {props.onPageChanged(p)}} className={props.currentPage === p && styles.selectedPage} > {p} </span>
+     })}
+     </div>
 
-
-    //create method render what return jsx 
-    render () {
-        //create pages counter and create pages area & округляємо до цілого
-        let pagesCount = Math.ceil (this.props.totalUsersCount/this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div className='app-wrapper app-wrapper-content'>
-           {/*робимо так щоб відображося виділення сторінки юзера що відкрита*/}
-           <div>
-            {pages.map (p => {
-                return  <span onClick = {() => {this.onPageChanged(p)}} className={this.props.currentPage === p && styles.selectedPage} > {p} </span>
-            })}
-            </div>
-
-           {//make areas from state - users
-            this.props.users.map (u => <div key = {u.id}>
-                {/* створюємо картинку юзера і кнопку фолов */}
-                <span>
-                    <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className = {styles.userPhoto} alt=""/>
-                    </div>
-                    <div>
-                        {/* створюємо переключателі фолов і анфолов за допомогою тернарних операторів */}
-                        { u.followed
-                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
-                    </div>
-                </span>
-                {/* створюємо області із іменем та статусом */}
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                {/* створюємо область та місто */}
-                <span>
-                    <div>{'u.location.country'}</div>
-                    <div>{'u.location.city'}</div>
-                </span>
-            </div>)
-        }
-    </div>
-}
+    {//make areas from state - users
+     props.users.map (u => <div key = {u.id}>
+         {/* створюємо картинку юзера і кнопку фолов */}
+         <span>
+             <div>
+                 <img src={u.photos.small != null ? u.photos.small : userPhoto} className = {styles.userPhoto} alt=""/>
+             </div>
+             <div>
+                 {/* створюємо переключателі фолов і анфолов за допомогою тернарних операторів */}
+                 { u.followed
+                 ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                 : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+             </div>
+         </span>
+         {/* створюємо області із іменем та статусом */}
+         <span>
+             <div>{u.name}</div>
+             <div>{u.status}</div>
+         </span>
+         {/* створюємо область та місто */}
+         <span>
+             <div>{'u.location.country'}</div>
+             <div>{'u.location.city'}</div>
+         </span>
+     </div>)
+ }
+</div>
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 //створюємо перемикач між сторінками юзерс
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+//create count of pages 
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState =  {
     users: [],
     //create users pages
     pageSize: 5,
-    totalUsersCount: 25,
+    totalUsersCount: 50,
     currentPage: 1
 };
 
@@ -43,6 +45,10 @@ const usersReducer = (state = initialState, action) => {
         case SET_CURRENT_PAGE: { 
             return {...state,currentPage: action.currentPage}
         }
+
+        case SET_TOTAL_USERS_COUNT: { 
+            return {...state, totalUsersCount: action.count}
+        }
             
         default:
                  return state
@@ -54,6 +60,7 @@ export const followActionCreator = (userID) => ({type: FOLLOW, userID })
 export const unfollowActionCreator = (userID) => ({type: UNFOLLOW, userID })
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users })
 export const setCurrentPageActionCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountActionCreator = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count:totalUsersCount })
 
 export default usersReducer;
 
