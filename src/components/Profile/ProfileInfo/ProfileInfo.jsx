ca07f7aa4a6e6acdad7432cf57ce5997d1f772ae
Maecenass <maecenasss@gmail.com>
2022-12-26 17:38:01 +0200

    create Status on Profile Page: profileStatus.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../Common/Preloader/Preloader';
+import ProfileStatus from './profileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -14,7 +15,7 @@ const ProfileInfo = (props) => {
       </div>
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} alt = '#' />
-      ava + description
+        <ProfileStatus status = {'Hello my friend'}/>
       </div>
     </div>
 
