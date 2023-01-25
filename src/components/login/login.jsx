950f071564e0e6244b243bbaf3ba48d3f4ddfd90
Maecenass <maecenasss@gmail.com>
2023-01-25 10:40:18 +0200

    Change FormControl & Login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { Input } from "../Common/Preloader/FormControl/FormControl";
+import { required } from "../../utils/validator";
 
 const LoginForm = (props) => {
     return (
                 <form onSubmit={props.handleSubmit}>
                     <div>
-                    <Field placeholder="Login" name="login" component={'input'} />
+                    <Field placeholder="Login" name="login" component={Input} validate={[required]} />
                     </div>
                     <div>
-                    <Field placeholder="Password" name="password" component = {'input'} />
+                    <Field placeholder="Password" name="password" component = {Input} validate={[required]} />
                     </div>
                     <div>
-                    <Field type={'checkbox'} name='remember me' component = {'input'}/> remember me
+                    <Field type={'checkbox'} name='remember me' component = {Input}/> remember me
                     </div>
                     <div>
                     <button>Login</button>
@@ -26,8 +28,7 @@ const LoginReduxForm = reduxForm ({form: 'login'})(LoginForm)
 const Login = (props) => {
     const onSubmit = (formData) => {
         console.log (formData)
-    }
-    
+    } 
     return <div>
                 <h1>LOGIN</h1>
                 <LoginReduxForm onSubmit={onSubmit}/>
