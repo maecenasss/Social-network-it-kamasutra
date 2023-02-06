0946c5489599e666c4903fcf5b9b6dacaadf2e9c
Maecenass <maecenasss@gmail.com>
2023-02-06 21:36:36 +0200

    useState for profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../Common/Preloader/Preloader';
-import ProfileStatus from './profileStatus';
+import ProfileStatusWithHooks from './profileStatusWithHooks';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
       </div>
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} alt = '#' />
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
       </div>
     </div>
 
