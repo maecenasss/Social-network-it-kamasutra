734ad2ffa3ab6858385c45e3546707162abd8b2f
Maecenass <maecenasss@gmail.com>
2023-02-20 23:37:32 +0200

    Use async, await to decrease syntax code instead .then in auth-reducer & profile-reduxer

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,4 @@
+import { Await } from "react-router-dom";
 import { stopSubmit } from "redux-form";
 import { authAPI } from "../components/Api/api";
 
@@ -29,37 +30,32 @@ const authReducer = (state = initialState, action) => {
 //ActionCreator - функція задача якої вернути об'єкт action
 export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}})
 
-export const getAuthUserData = () => (dispatch) => {
-    return authAPI.me()
-        .then(response => {  
-            if (response.data.resultCode === 0) {
-                let {id, email, login} = response.data.data;
-                dispatch (setAuthUserData (id, email, login, true))
-                }
-        });
-}
+export const getAuthUserData = () => async (dispatch) => {
+    let response = await authAPI.me();
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-    authAPI.login(email, password, rememberMe)
+    if (response.data.resultCode === 0) {
+            let {id, email, login} = response.data.data;
+            dispatch (setAuthUserData (id, email, login, true))
+    }
+};
+
+export const login = (email, password, rememberMe) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe);
     //якщо логіі і код є правильними, то отримуємо дані юзера із сервера
-    .then(response => {  
     if (response.data.resultCode === 0) {
           dispatch (getAuthUserData())
-        } else {
-            let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
-            dispatch(stopSubmit ('login', {_error: message}));
-        }
-    });
+    } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
+        dispatch(stopSubmit ('login', {_error: message}));
+        };
 }
 
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(response => {  
+export const logout = () => async (dispatch) => {
+    let response = await authAPI.logout();
 //якщо вилогінюємся то установлюються нульові значення ід, емеіла і логіна
      if (response.data.resultCode === 0) {
           dispatch (setAuthUserData(null, null, null, false))
-        }
-    });
+        };
 }
 
 export default authReducer;
